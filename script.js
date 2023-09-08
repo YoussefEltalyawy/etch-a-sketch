c657547ebc05eea5e879fda9656c8c75205993cb
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2023-09-08 10:16:10 +0300

    Fixed The girdSizeBtn now works but still bug occurs when size is 64x64 , refactored most of the code.

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const mainGrid = document.querySelector('.main-grid');
+const mainGrid = document.querySelector(".main-grid");
+const gridSizeBtn = document.querySelector(".gs-btn");
+
 let gridSquares;
+let gridSize;
+let overallSquares;
 
-// function startAll () {
-
-// }
-
-let sps = 256;
-function initialzingGrid() {
-  for(let i = 0; i < sps; i++) {
-    gridSquares = document.createElement('div');
-    gridSquares.className = 'grid-squares';
+function initializeGrid() {
+  for (let i = 0; i < overallSquares; i++) {
+    gridSquares = document.createElement("div");
+    gridSquares.className = "grid-squares";
     mainGrid.appendChild(gridSquares);
-    gridSquares.addEventListener('mouseover', function() {
-      this.style.backgroundColor = 'pink';
+    gridSquares.style.width = `${680 / gridSize}px`;
+    gridSquares.style.height = `${680 / gridSize}px`;
+    gridSquares.addEventListener("mouseover", function () {
+      this.style.backgroundColor = "pink";
     });
   }
-  
 }
-initialzingGrid();
+
+gridSizeBtn.addEventListener("click", function () {
+  gridSize = prompt("Select Grid Size 16,32,64,...etc");
+  overallSquares = gridSize * gridSize;
+  initializeGrid();
+});
